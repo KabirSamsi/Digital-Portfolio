cb549da55097fccf7262357cfda3db8d0161675c
KabirSamsi <35539143+KabirSamsi@users.noreply.github.com>
2018-09-24 11:44:17 -0700

    Add files via upload

---
+++ bootstrap portfolio.js
@@ -0,0 +1,60 @@
+const home = document.querySelector('#home');
+const cy1 = document.getElementById('cy1');
+const cy2 = document.getElementById('cy2');
+const cy3 = document.getElementById('cy3');
+const cy4 = document.querySelector('#cy4');
+const cy5 = document.querySelector('#cy5');
+
+home.addEventListener('mouseover', () => {
+  home.style.color = 'blue';
+})
+
+home.addEventListener('mouseout', () => {
+  home.style.color = 'black';
+
+})
+
+cy1.addEventListener('mouseover', () => {
+  cy1.style.color = 'blue';
+})
+
+cy1.addEventListener('mouseout', () => {
+  cy1.style.color = 'black';
+
+})
+
+cy2.addEventListener('mouseover', () => {
+  cy2.style.color = 'blue';
+})
+
+cy2.addEventListener('mouseout', () => {
+  cy2.style.color = 'black';
+
+})
+
+cy3.addEventListener('mouseover', () => {
+  cy3.style.color = 'blue';
+})
+
+cy3.addEventListener('mouseout', () => {
+  cy3.style.color = 'black';
+
+})
+
+cy4.addEventListener('mouseover', () => {
+  cy4.style.color = 'blue';
+})
+
+cy4.addEventListener('mouseout', () => {
+  cy4.style.color = 'black';
+
+})
+
+cy5.addEventListener('mouseover', () => {
+  cy5.style.color = 'blue';
+})
+
+cy5.addEventListener('mouseout', () => {
+  cy5.style.color = 'black';
+
+})
